8dc0a66db7d4a25db7027763eeeb7494d842020f
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-07-06 16:18:32 -0700

    added environ

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -17,6 +17,8 @@ from pathlib import Path
 import dj_database_url
 
 import environ
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
